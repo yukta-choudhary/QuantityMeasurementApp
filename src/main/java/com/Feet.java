b8ee059dd482cde45cf7fb9aa86cedf396500dc2
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-02-23 09:15:45 +0530

    [Yukta]: added Feet

---
+++ src/main/java/com/Feet.java
@@ -0,0 +1,44 @@
+package com;
+
+public class Feet {
+
+    private final double value;
+
+    // Constructor
+    public Feet(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    // Override equals method
+    @Override
+    public boolean equals(Object obj) {
+
+        // 1. Reference check
+        if (this == obj) {
+            return true;
+        }
+
+        // 2. Null check
+        if (obj == null) {
+            return false;
+        }
+
+        // 3. Type check
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        // 4. Cast and compare values
+        Feet other = (Feet) obj;
+        return Double.compare(this.value, other.value) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Double.hashCode(value);
+    }
+}

d20146089fa7e79a0036f0517d1448e987c875d0
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-02-23 12:21:09 +0530

    [Yukta]: modified Length

--- src/main/java/com/Length.java
+++ src/main/java/com/Length.java
@@ -9,8 +9,10 @@ public class Length {
 
     // Enum for unit types and conversion factors
     public enum LengthUnit {
-        FEET(12.0),   // 1 foot = 12 inches
-        INCHES(1.0);  // base unit
+        FEET(12.0),        // 1 foot = 12 inches
+        INCHES(1.0),       // base unit
+        YARDS(36.0),       // 1 yard = 36 inches
+        CENTIMETERS(0.393701); // 1 cm = 0.393701 inches
 
         private final double conversionFactor;
 

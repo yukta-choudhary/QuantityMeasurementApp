01ac2622e59a0fc9db982b1ec3677b2200767af6
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-02-23 09:14:23 +0530

    [Yukta]: added QuantityMeasurementApp

---
+++ src/main/java/com/QuantityMeasurementApp.java
@@ -0,0 +1,13 @@
+package com;
+
+public class QuantityMeasurementApp {
+
+    public static void main(String[] args) {
+
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        System.out.println("Are equal? " + f1.equals(f2));
+    }
+}
+

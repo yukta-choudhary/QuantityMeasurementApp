c8edd39b1b7f2005d45eef4f16742869fed55217
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-02-23 22:55:51 +0530

    [Yukta]: modified QuantityMeasurementApp

--- src/main/java/com/QuantityMeasurementApp.java
+++ src/main/java/com/QuantityMeasurementApp.java
@@ -2,40 +2,11 @@ package com;
 
 public class QuantityMeasurementApp {
 
-    
-    public static boolean demonstrateLengthEquality(Length l1, Length l2) {
-        return l1.equals(l2);
+	public static void main(String[] args) {
+
+        Length length1 = new Length(1.0, LengthUnit.FEET);
+        Length length2 = new Length(12.0, LengthUnit.INCHES);
+
+        System.out.println("Are lengths equal? " + length1.equals(length2));
     }
-
-    public static void demonstrateFeetEquality() {
-        Length f1 = new Length(1.0, Length.LengthUnit.FEET);
-        Length f2 = new Length(1.0, Length.LengthUnit.FEET);
-
-        System.out.println("Feet equal? " + f1.equals(f2));
-    }
-
-    public static void demonstrateInchesEquality() {
-        Length i1 = new Length(1.0, Length.LengthUnit.INCHES);
-        Length i2 = new Length(1.0, Length.LengthUnit.INCHES);
-
-        System.out.println("Inches equal? " + i1.equals(i2));
-    }
-
-    public static void demonstrateFeetInchesComparison() {
-        Length f = new Length(1.0, Length.LengthUnit.FEET);
-        Length i = new Length(12.0, Length.LengthUnit.INCHES);
-
-        System.out.println("Feet and Inches equal? " + f.equals(i));
-    }
-
-
-    public static void main(String[] args) {
-    	demonstrateFeetEquality();
-    	demonstrateInchesEquality();
-        demonstrateFeetInchesComparison();
-    }
-
 }
-
-
-    

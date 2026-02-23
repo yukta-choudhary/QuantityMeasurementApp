0c52772a021c0b084b6425bda39cc85fdc4c4ff5
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-02-23 12:21:39 +0530

    [Yukta]: modified QuantityMeasurementAppTest

--- src/test/java/com/QuantityMeasurementAppTest.java
+++ src/test/java/com/QuantityMeasurementAppTest.java
@@ -58,4 +58,43 @@ public class QuantityMeasurementAppTest {
 
         assertTrue(f1.equals(f1));
     }
+ // UC4: Yard to Yard
+    @Test
+    public void yardEquality() {
+        Length y1 = new Length(1.0, Length.LengthUnit.YARDS);
+        Length y2 = new Length(1.0, Length.LengthUnit.YARDS);
+        assertTrue(y1.equals(y2));
+    }
+
+    // UC4: Yard to Feet
+    @Test
+    public void yardEqualsFeet() {
+        Length y = new Length(1.0, Length.LengthUnit.YARDS);
+        Length f = new Length(3.0, Length.LengthUnit.FEET);
+        assertTrue(y.equals(f));
+    }
+
+    // UC4: Yard to Inches
+    @Test
+    public void yardEqualsInches() {
+        Length y = new Length(1.0, Length.LengthUnit.YARDS);
+        Length i = new Length(36.0, Length.LengthUnit.INCHES);
+        assertTrue(y.equals(i));
+    }
+
+    // UC4: Centimeter to Inches
+    @Test
+    public void centimeterEqualsInches() {
+        Length cm = new Length(1.0, Length.LengthUnit.CENTIMETERS);
+        Length in = new Length(0.393701, Length.LengthUnit.INCHES);
+        assertTrue(cm.equals(in));
+    }
+
+    // UC4: Inequality
+    @Test
+    public void centimeterNotEqualFeet() {
+        Length cm = new Length(1.0, Length.LengthUnit.CENTIMETERS);
+        Length f = new Length(1.0, Length.LengthUnit.FEET);
+        assertFalse(cm.equals(f));
+    }
 }
